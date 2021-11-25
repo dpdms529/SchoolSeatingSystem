fa8a5193b31a52acde262ff595b11292fbc2f40c
YE-EUN <musiclove529@naver.com>
2021-11-25 15:20:58 +0900

    create SchoolSeatingSystem.sv

---
+++ SchoolSeatingSystem.sv
@@ -0,0 +1,19 @@
+module SchoolSeatingSystem(input logic clk,
+                           input logic [24:0] Student_NO,
+                           input logic [4:0] Seat_No,
+                           input logic write,
+                           output logic [10:0] time_out
+                           output logic [1:0] Seat_State);
+
+   logic [10:0] setted_reset_time, setted_limit_time, Time;
+   logic rst_temp, Do_Not_Seat_sig;
+   
+   assign time_out = Time;
+   
+   set_up S0(setted_reset_time, setted_limit_time);
+   Timer T0(clk, setted_reset_time, rst_temp, Time);
+   mem1 M1(clk, write, Student_No, Seat_No);
+   mem2 M2(clk, rst_temp, write, Time, Seat_State, Seat_No, setted_limit_time, Do_Not_Seat_sig);
+   
+   
+endmodule
