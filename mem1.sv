67a0b5cc57018da5f4998eb1fa801c4b45524cdb
yegrina <syn5867@naver.com>
2021-11-25 18:03:35 +0900

    update mem1

--- mem1.sv
+++ mem1.sv
@@ -3,14 +3,14 @@ module mem1(input logic clk_mem1,
 				input logic [24:0] Student_No_mem1,
 				input logic [7:0] Seat_No_mem1);
 				
-	typedef logic [31:0] Student_Num_Table [0:31];
-	Student_Num_Table RAM_DATA = '{ 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 
-							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
-							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
-							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
-							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
-							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
-							32'b0, 32'b0};
+	typedef logic [24:0] Student_Num_Table [0:31];
+	Student_Num_Table RAM_DATA = '{ 24'b0, 24'b0, 24'b0, 24'b0, 24'b0, 
+							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
+							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
+							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
+							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
+							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
+							24'b0, 24'b0};
 	
 	always_ff @(Student_No_mem1 or Seat_No_mem1 or write_mem1) begin
 		if (write_mem1 === 1) begin
