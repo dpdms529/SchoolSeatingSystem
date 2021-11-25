d8f4273e905921545982abeb9bb0036136db7ce9
yegrina <syn5867@naver.com>
2021-11-25 15:25:29 +0900

    create mem1

---
+++ mem1.sv
@@ -0,0 +1,22 @@
+module mem1(input logic clk_mem1,
+				input logic write_mem1,
+				input logic [24:0] Student_No_mem1,
+				input logic [7:0] Seat_No_mem1);
+				
+	typedef logic[31:0] Student_Num_Table [0:31];
+	Student_Num_Table RAM_DATA;
+	assign RAM_DATA = {32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 
+							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
+							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
+							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
+							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
+							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
+							32'b0, 32'b0};
+	
+	always_ff @(Student_No_mem1 or Seat_No_mem1 or write_mem1) begin
+		if(write_mem1 = 1) begin
+//			RAM_DATA[Seat_No_mem1] <= Student_No_mem1;	// 입력받은 자리에 학번을 할당
+		end
+	end
+	
+endmodule
