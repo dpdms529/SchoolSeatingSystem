47b377232123d0b85d8e97be11b633290cfbfa2f
yegrina <syn5867@naver.com>
2021-11-25 18:05:11 +0900

    update mem1

--- mem1.sv
+++ mem1.sv
@@ -4,13 +4,13 @@ module mem1(input logic clk_mem1,
 				input logic [7:0] Seat_No_mem1);
 				
 	typedef logic [24:0] Student_Num_Table [0:31];
-	Student_Num_Table RAM_DATA = '{ 24'b0, 24'b0, 24'b0, 24'b0, 24'b0, 
-							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
-							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
-							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
-							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
-							24'b0, 24'b0, 24'b0, 24'b0, 24'b0,
-							24'b0, 24'b0};
+	Student_Num_Table RAM_DATA = '{ 25'b0, 25'b0, 25'b0, 25'b0, 25'b0, 
+							25'b0, 25'b0, 25'b0, 25'b0, 25'b0,
+							25'b0, 25'b0, 25'b0, 25'b0, 25'b0,
+							25'b0, 25'b0, 25'b0, 25'b0, 25'b0,
+							25'b0, 25'b0, 25'b0, 25'b0, 25'b0,
+							25'b0, 25'b0, 25'b0, 25'b0, 25'b0,
+							25'b0, 25'b0};
 	
 	always_ff @(Student_No_mem1 or Seat_No_mem1 or write_mem1) begin
 		if (write_mem1 === 1) begin
