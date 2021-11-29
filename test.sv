a5c3dc8df8b36f04a060034b5785def0b716ef14
YE-EUN <musiclove529@naver.com>
2021-11-29 15:15:05 +0900

    test.sv

--- test.sv
+++ test.sv
@@ -8,23 +8,25 @@ module test();
 	
 	SchoolSeatingSystem C0(clk_tb, Student_No_tb, Seat_No_tb, write_tb, Time, Seat_State_tb);
 	
+	always #5 clk_tb <= ~clk_tb;
+	
 	initial begin
-		clk_tb <= ~clk_tb; #5;
-		write_tb <= 1; #40;
-		Student_No_tb <= 25'b1111111111111111111111111; #50;
-		Student_No_tb <= 25'b1111011000001000011110011; #80;
-		Student_No_tb <= 25'b1111001111100111111001111; #110;
-		Student_No_tb <= 25'b1111111111111111111111111; #120;
+		#40 write_tb <= 1;
+		#10 Student_No_tb <= 25'b1111111111111111111111111;
+		#40 Student_No_tb <= 25'b1111011000001000011110011;
+		#70 Student_No_tb <= 25'b1111001111100111111001111;
+		#80 Student_No_tb <= 25'b1111111111111111111111111;
 		
-		Seat_No_tb <= 5'b00001; #50;
-		Seat_No_tb <= 5'b00010; #80;
-		Seat_No_tb <= 5'b00101; #110;
-		Seat_No_tb <= 5'b00001; #120;
+		#10 Seat_No_tb <= 5'b00001;
+		#40 Seat_No_tb <= 5'b00010;
+		#70 Seat_No_tb <= 5'b00101;
+		#80 Seat_No_tb <= 5'b00001; 
 		
-		Seat_State_tb <= 2'b11; #50;
-		Seat_State_tb <= 2'b11; #80;
-		Seat_State_tb <= 2'b11; #110;
-		Seat_State_tb <= 2'b01; #120;
-	end
+		#10 Seat_State_tb <= 2'b11;
+		#40 Seat_State_tb <= 2'b11;
+		#70 Seat_State_tb <= 2'b11;
+		#80 Seat_State_tb <= 2'b01;
+	end	
+		
 	
 endmodule
