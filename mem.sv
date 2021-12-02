50d72eb1ab0e937babb97031798b91ef09376bb3
yegrina <syn5867@naver.com>
2021-12-02 18:31:29 +0900

    delete typedef in mem.sv

--- mem.sv
+++ mem.sv
@@ -7,8 +7,7 @@ module mem(input logic rst_mem,
 				input logic [10:0] limit_time,
 				output logic Do_Not_Seat);
 				
-	typedef logic [31:0] Student_Num_Table [0:31];
-	Student_Num_Table RAM_DATA = '{ 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 
+	logic [31:0] Student_Num_Table [0:31] = '{ 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 
 							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
 							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
 							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
@@ -16,16 +15,15 @@ module mem(input logic rst_mem,
 							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
 							32'b0, 32'b0};
 				
-	typedef logic [10:0] Time_Table [0:31];
-	Time_Table RAM_DATA1 = '{ 11'b0, 11'b0, 11'b0, 11'b0, 11'b0, 
+	logic [10:0] Time_Table [0:31] = '{ 11'b0, 11'b0, 11'b0, 11'b0, 11'b0, 
 									11'b0, 11'b0, 11'b0, 11'b0, 11'b0,
 									11'b0, 11'b0, 11'b0, 11'b0, 11'b0,
 									11'b0, 11'b0, 11'b0, 11'b0, 11'b0,
 									11'b0, 11'b0, 11'b0, 11'b0, 11'b0,
 									11'b0, 11'b0, 11'b0, 11'b0, 11'b0,
 									11'b0, 11'b0};
-	typedef logic [1:0] Seat_State_Table [0:31];
-	Seat_State_Table RAM_DATA2 = '{ 2'b0, 2'b0, 2'b0, 2'b0, 2'b0, 
+
+	logic [1:0] Seat_State_Table [0:31] = '{ 2'b0, 2'b0, 2'b0, 2'b0, 2'b0, 
 											2'b0, 2'b0, 2'b0, 2'b0, 2'b0,
 											2'b0, 2'b0, 2'b0, 2'b0, 2'b0,
 											2'b0, 2'b0, 2'b0, 2'b0, 2'b0,
@@ -36,19 +34,19 @@ module mem(input logic rst_mem,
 	
 	always_comb begin
 		for (int i=0; i<32; i++) begin
-			Time_temp = Time_mem-RAM_DATA1[i];
-			if (RAM_DATA2[i] === 1 && Time_temp > limit_time) begin
-				RAM_DATA[i] <= 0;
-				RAM_DATA1[i] <= 0;
-				RAM_DATA2[i] <= 0;
+			Time_temp = Time_mem - Time_Table[i];
+			if (Seat_State_Table[i] === 1 && Time_temp > limit_time) begin
+				Student_Num_Table[i] <= 0;
+				Time_Table[i] <= 0;
+				Seat_State_Table[i] <= 0;
 			end
 		end
 		
-		if (RAM_DATA2[Seat_No_mem] === 2 && Seat_State_mem === 2) Do_Not_Seat = 1;
+		if (Seat_State_Table[Seat_No_mem] === 2 && Seat_State_mem === 2) Do_Not_Seat = 1;
 		else Do_Not_Seat = 0;
 		
 		if (rst_mem === 1) begin
-			RAM_DATA <= '{ 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 
+			Student_Num_Table <= '{ 32'b0, 32'b0, 32'b0, 32'b0, 32'b0, 
 							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
 							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
 							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
@@ -56,7 +54,7 @@ module mem(input logic rst_mem,
 							32'b0, 32'b0, 32'b0, 32'b0, 32'b0,
 							32'b0, 32'b0};
 							
-			RAM_DATA <= '{ 11'b0, 11'b0, 11'b0, 11'b0, 11'b0, 
+			Time_Table <= '{ 11'b0, 11'b0, 11'b0, 11'b0, 11'b0, 
 								11'b0, 11'b0, 11'b0, 11'b0, 11'b0,
 								11'b0, 11'b0, 11'b0, 11'b0, 11'b0,
 								11'b0, 11'b0, 11'b0, 11'b0, 11'b0,
@@ -64,7 +62,7 @@ module mem(input logic rst_mem,
 								11'b0, 11'b0, 11'b0, 11'b0, 11'b0,
 								11'b0, 11'b0};
 		
-			RAM_DATA2 <= '{ 2'b0, 2'b0, 2'b0, 2'b0, 2'b0, 
+			Seat_State_Table <= '{ 2'b0, 2'b0, 2'b0, 2'b0, 2'b0, 
 								2'b0, 2'b0, 2'b0, 2'b0, 2'b0,
 								2'b0, 2'b0, 2'b0, 2'b0, 2'b0,
 								2'b0, 2'b0, 2'b0, 2'b0, 2'b0,
@@ -74,9 +72,9 @@ module mem(input logic rst_mem,
 		end
 		
 		if (write_mem === 1 && Do_Not_Seat !== 1) begin
-			RAM_DATA[Seat_No_mem] <= Student_No_mem;
-			RAM_DATA1[Seat_No_mem] <= Time_mem;
-			RAM_DATA2[Seat_No_mem] <= Seat_State_mem;
+			Student_Num_Table[Seat_No_mem] <= Student_No_mem;
+			Time_Table[Seat_No_mem] <= Time_mem;
+			Seat_State_Table[Seat_No_mem] <= Seat_State_mem;
 		end
 			
 	end
